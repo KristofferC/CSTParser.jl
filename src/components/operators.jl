96c3864f9d3db9e0dc8c4417c415594d9d9ce95e
ZacLN <zac.nugent@gmail.com>
2017-08-04 15:44:06 +0000

    wip

--- src/components/operators.jl
+++ src/components/operators.jl
@@ -185,7 +185,7 @@ function parse_unary(ps::ParseState, op::EXPR{OPERATOR{P,K,dot}}) where {P, K, d
     if issyntaxunarycall(op)
         ret = EXPR{UnarySyntaxOpCall}(EXPR[op, arg], "")
     else
-        ret = EXPR{Call}(EXPR[op, arg], "")
+        ret = EXPR{UnaryOpCall}(EXPR[op, arg], "")
     end
     return ret
 end
